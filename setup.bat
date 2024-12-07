e547eca1cb18178880ec258c227e18e689153a5b
bandit-1200 <57681283+bandit-1200@users.noreply.github.com>
2024-12-07 14:01:02 +0100

    Entfernen unerwünschter Dateien und Aktualisierung der .gitignore

--- setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "C:\Program Files (x86)\Inno Setup 6"
-@REM ISCC.exe "C:\Users\marco\Documents\code\BiMaDock\BiMaDock_local.iss"
-ISCC.exe "C:\Users\Marco\Documents\VisualStudioCode\BiMaDock\BiMaDock_local.iss"
-pause
