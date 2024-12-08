a760d5252720fa8de0002d2df419e98fc5fc539c
bandit-1200 <57681283+bandit-1200@users.noreply.github.com>
2024-12-08 10:14:27 +0100

    Anpassung Autostart Methode

--- StartupManager.cs
+++ StartupManager.cs
@@ -11,6 +11,9 @@ namespace BiMaDock
 
         public static void AddToStartup(bool isChecked)
         {
+            // Debugging-Ausgabe zur Überprüfung des Pfads
+            MessageBox.Show($"DEBUG: appPath = {appPath}");
+
             using (Microsoft.Win32.RegistryKey? key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
             {
                 if (key == null)
